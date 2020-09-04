00259e09fea8d0d9496447b3a6adf243597bb697
chan227 <xiang@graduate.utm.my>
2020-09-04 23:46:44 +0800

    init_project

---
+++ config/application.rb
@@ -0,0 +1,19 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module MalayGroup1
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 6.0
+
+    config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')] # it will load all file at locales folder and, only read rm anf yml file for i18n
+
+    config.i18n.default_locale = :en # language default project
+    I18n.available_locales = [:en] # define all laguage that you project allow
+  end
+end
