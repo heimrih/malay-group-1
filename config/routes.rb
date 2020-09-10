2238dbb3bd2f62ced30c222672b28daf3f9fb85e
EvonneNgXinYi <evonne1997@gmail.com>
2020-09-10 15:28:25 +0800

    home layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /en/ do
+    root "static_pages#home"
+    get "/help", to: "static_pages#help"
+    get "/about", to: "static_pages#about"
+    get "/signup", to: "users#new"
+    get "users/new"
   end
 end
