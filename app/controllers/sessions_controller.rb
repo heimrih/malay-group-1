d57921ab10208f653fb5a11e169136c06d37e3c8
EvonneNgXinYi <evonne1997@gmail.com>
2020-09-16 13:13:54 +0800

    Setting Setup

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     if user&.authenticate params[:session][:password]
       log_in user
       params[:session][:remember_me].eql? Settings.collections.session_controller_create ? remember(user) : forget(user)
-      redirect_to user
+      redirect_back_or user
     else
       flash.now[:danger] = t "sessions.create.fail_mess_session"
       render :new
